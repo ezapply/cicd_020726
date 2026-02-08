120bf8786d3712f1f66f67e3566394fe20553302
ezapply <92282385+ezapply@users.noreply.github.com>
2026-02-07 17:48:29 -0800

    Create test_calc.py
    this will test calc.py for CICD

---
+++ test_calc.py
@@ -0,0 +1,11 @@
+from calc import add
+
+def test_add():
+    # This is our "Unit Test"
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    print("All tests passed!")
+
+if __name__ == "__main__":
+    test_add()
+  

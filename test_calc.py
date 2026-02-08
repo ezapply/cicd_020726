5c218b796635d22e39c713912ff0a9f440cf15b0
Eric Chan <chanhaylick@gmail.com>
2026-02-08 10:46:05 -0800

    intentionally test wrong

--- test_calc.py
+++ test_calc.py
@@ -4,6 +4,7 @@ def test_add():
     # This is our "Unit Test"
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
+    assert add(10, 12) == 23
     print("All tests passed!")
 
 if __name__ == "__main__":
